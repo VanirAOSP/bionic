21296347df27ece49796ea1397e7fb87557c426e
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2012-12-14 19:09:07 +0100

    bionic: Make use of new memcpy, tweak settings
    The new memcpy() is slightly faster than the cortex-strings version,
    and fixes the Galaxy Nexus camera issue.
    
    Change-Id: Ie65d14992a399e9bc2709a3d710d52b5b1a62e87
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- libc/Android.mk
+++ libc/Android.mk
@@ -378,6 +378,7 @@ libc_common_src_files += \
 	arch-arm/bionic/tgkill.S \
 	arch-arm/bionic/memcmp.S \
 	arch-arm/bionic/memcmp16.S \
+	arch-arm/bionic/memcpy.S \
 	arch-arm/bionic/setjmp.S \
 	arch-arm/bionic/sigsetjmp.S \
 	arch-arm/bionic/strcmp.S \
@@ -410,16 +411,13 @@ libc_arch_dynamic_src_files := \
 ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
 libc_common_src_files += \
 	arch-arm/bionic/armv7/memchr.S \
-	arch-arm/bionic/armv7/memcpy.S \
 	arch-arm/bionic/armv7/memset.S \
-	arch-arm/bionic/armv7/bzero.S \
 	arch-arm/bionic/armv7/strchr.S \
 	arch-arm/bionic/armv7/strcpy.c \
 	arch-arm/bionic/armv7/strlen.S
 else
 libc_common_src_files += \
 	string/memchr.c \
-	arch-arm/bionic/memcpy.S \
 	arch-arm/bionic/memset.S \
 	string/strchr.c \
 	arch-arm/bionic/strcpy.S \
@@ -574,6 +572,9 @@ ifeq ($(TARGET_ARCH),arm)
   endif
   ifeq ($(ARCH_ARM_USE_NON_NEON_MEMCPY),true)
     libc_common_cflags += -DARCH_ARM_USE_NON_NEON_MEMCPY
+  endif
+  ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
+    libc_common_cflags += -DNEON_UNALIGNED_ACCESS -DNEON_MEMCPY_ALIGNMENT_DIVIDER=224
   endif
 endif # !arm
 
