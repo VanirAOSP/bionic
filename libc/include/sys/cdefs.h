burstlam/android_bionic

Add workarounds for current compilers
With current compilers, we're getting "this function cannot be inlined"
errors on vsnprintf and strlcpy because the always_inline attribute is
set.

Before marking this a "fix" instead of a "workaround", we should figure
out why the functions can't be inlined.

Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

libc/include/sys/cdefs.h
@@ -522,6 +522,10 @@
     __attribute__ ((always_inline)) \
     __attribute__ ((gnu_inline)) \
     __attribute__ ((artificial))
+#define __BIONIC_FORTIFY_INLINE_WITHOUT_ALWAYS \
+    extern inline \
+    __attribute__ ((gnu_inline)) \
+    __attribute__ ((artificial))
 #endif
 #define __BIONIC_FORTIFY_UNKNOWN_SIZE ((size_t) -1)
 

VanirAOSP/bionic

libc/include/sys/cdefs.h
@@ -538,6 +538,9 @@
     extern inline \
     __attribute__ ((always_inline)) \
     __attribute__ ((gnu_inline))
+#define __BIONIC_FORTIFY_INLINE_WITHOUT_ALWAYS \
+    extern inline \
+    __attribute__ ((gnu_inline))
 #endif
 #define __BIONIC_FORTIFY_UNKNOWN_SIZE ((size_t) -1)
 