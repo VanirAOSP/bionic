33d702a0ab4a30f515f156c6cda4b1e01b488f46
nuclearmistake <nuclearmistake@gmail.com>
2013-11-16 14:19:22 -0500

    memchr: handle arm's memchr inside arch-arm/generic/generic.mk, so tegra2 can replace it
    Change-Id: I789465337f7a1af21a1c3cfa6fb0191cfdedbdfd

--- libc/arch-arm/generic/generic.mk
+++ libc/arch-arm/generic/generic.mk
@@ -1,3 +1,4 @@
+$(call libc-add-cpu-variant-src,MEMCHR,bionic/memchr.c)
 $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/generic/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/generic/bionic/memset.S)
 $(call libc-add-cpu-variant-src,MEMMOVE,bionic/memmove.c.arm)
