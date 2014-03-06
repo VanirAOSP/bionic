21542a5e83875c1f8adc5c50e93d83637ed4e392
synergy dev <synergye@codefi.re>
2014-03-06 08:07:11 -0800

    [ARM NEON] libm: original optimized modf
    Summary: Significantly faster than the generic C implementation
    
    Test Plan: test on a device that had issues with libm neon based modf implementation.
    
    Reviewers: anders3408, jriordan001, raymanfx, randall.rushing
    
    Reviewed By: randall.rushing
    
    CC: raymanfx
    
    Differential Revision: http://review.codefirex.com/D496
    
    Conflicts:
    
    	libm/Android.mk
    
    Change-Id: I59902f8f194961316a7f525e511806baa7eb5232

--- libm/Android.mk
+++ libm/Android.mk
@@ -142,7 +142,6 @@ libm_common_src_files += \
     upstream-freebsd/lib/msun/src/s_lrintf.c \
     upstream-freebsd/lib/msun/src/s_lround.c \
     upstream-freebsd/lib/msun/src/s_lroundf.c \
-    upstream-freebsd/lib/msun/src/s_modf.c \
     upstream-freebsd/lib/msun/src/s_modff.c \
     upstream-freebsd/lib/msun/src/s_nan.c \
     upstream-freebsd/lib/msun/src/s_nearbyint.c \
@@ -248,6 +247,14 @@ else
       upstream-freebsd/lib/msun/src/e_sqrtf.c \
       upstream-freebsd/lib/msun/src/e_sqrt.c
     endif
+endif
+
+ifeq ($(ARCH_ARM_HAVE_NEON),true)
+  libm_arm_src_files += \
+    arm/s_modf.S
+else
+  libm_common_src_files += \
+    upstream-freebsd/lib/msun/src/s_modf.c
 endif
 
 libm_x86_includes := $(LOCAL_PATH)/i386 $(LOCAL_PATH)/i387
