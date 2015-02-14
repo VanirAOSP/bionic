795e6408e992432056e82e85c75f6a8271710210
Dmitriy Ivanov <dimitry@google.com>
2015-02-13 16:29:50 -0800

    Fix: DT_DEBUG was acting as DT_REL on mips64
    Change-Id: I88827aa07d75d06d606c987e6270fcca3ae6216f

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -1969,8 +1969,8 @@ bool soinfo::PrelinkImage() {
         if ((dynamic_flags & PF_W) != 0) {
           d->d_un.d_val = reinterpret_cast<uintptr_t>(&_r_debug);
         }
-        break;
 #endif
+        break;
 #if defined(USE_RELA)
       case DT_RELA:
         rela = reinterpret_cast<ElfW(Rela)*>(load_bias + d->d_un.d_ptr);
